c35afb66d32774d740cfe1bd849335950f25a7ee
Thomas Haas <github@thomashaas.de>
2025-01-08 08:47:21 +0100

    GenerateTaskCommandTest hinzugefügt

---
+++ tests/Command/GenerateTaskCommandTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests\Command;
+
+use App\Command\GenerateTaskCommand;
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Console\Application;
+
+class GenerateTaskCommandTest extends TestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testExecute()
+    {
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $entityManager->expects($this->once())
+            ->method('persist')
+            ->with($this->isInstanceOf(Task::class));
+        $entityManager->expects($this->once())
+            ->method('flush');
+
+        $command = new GenerateTaskCommand($entityManager);
+
+        $application = new Application();
+        $application->add($command);
+
+        $commandTester = new CommandTester($application->find('app:generate-task'));
+        $commandTester->execute([]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Task created successfully!', $output);
+        $this->assertEquals(0, $commandTester->getStatusCode());
+    }
+}
