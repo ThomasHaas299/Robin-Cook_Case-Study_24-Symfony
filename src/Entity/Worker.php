872ede4260b117beae867afa3d3335a28b6afd80
Thomas Haas <github@thomashaas.de>
2025-01-08 10:11:03 +0100

    Authentication funktioniert, jetzt kann der TaskController implementiert werden

--- src/Entity/Worker.php
+++ src/Entity/Worker.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use App\Repository\WorkerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Random\RandomException;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: WorkerRepository::class)]
-class Worker
+class Worker implements UserInterface
 {
 
     #[ORM\Id]
@@ -16,13 +18,41 @@ class Worker
     private ?string $id = null;
 
     #[ORM\Column]
-    public bool $disabled = false;
+    private bool $disabled = false;
 
     #[ORM\Column(nullable: false)]
-    public string $accessToken;
+    private string $accessToken;
 
+    /**
+     * @throws RandomException
+     */
     public function __construct()
     {
         $this->accessToken = bin2hex(random_bytes(32));
+    }
+
+    public function isDisabled(): bool
+    {
+        return $this->disabled;
+    }
+
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+
+
+    public function getRoles(): array
+    {
+        return ['ROLE_WORKER'];
+    }
+
+    public function eraseCredentials(): void
+    {
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->id;
     }
 }
---
+++ src/Security/WorkerAuthenticator.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\WorkerRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Throwable;
+
+class WorkerAuthenticator extends AbstractAuthenticator
+{
+
+    public function __construct(private readonly WorkerRepository $workerRepository)
+    {
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        // return $request->headers->has('X-Worker-Id') && $request->headers->has('X-Worker-Token');
+        // Supports all /api/-requests
+        return str_starts_with($request->getPathInfo(), '/api/');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        if (!($request->headers->has('X-Worker-Id') && $request->headers->has('X-Worker-Token'))) {
+            throw new AuthenticationException('No authentication provided');
+        }
+
+        $workerId = $request->headers->get('X-Worker-Id');
+        $workerToken = $request->headers->get('X-Worker-Token');
+
+        $userBadgeCallback = function ($workerId) use ($workerToken) {
+            return $this->workerRepository->findOneBy(
+                [
+                    'id' => $workerId,
+                    'disabled' => 0,
+                    'accessToken' => $workerToken]
+            );
+        };
+
+        return new SelfValidatingPassport(
+            new UserBadge($workerId, $userBadgeCallback)
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // successful authentication, nothing to do
+        return null;
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        // authentication failure, the exception is thrown
+        throw $exception;
+    }
+}
